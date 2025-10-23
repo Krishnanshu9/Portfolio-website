6e7686b29e20f8ffdd0551e1278b16e5031b53c5
Krishnanshu9 <krishnanshuchaudhary@gmail.com>
2025-10-23 05:36:17 +0530

    Default theme change

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setDarkMode] = useState(false);
+    const [isDarkMode, setDarkMode] = useState(true);
     // useEffect is used for "Do something after render, and optionally clean it up."
     useEffect(()=>{
         const storedTheme = localStorage.getItem("theme");

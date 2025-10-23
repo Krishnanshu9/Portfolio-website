8316bfb33c25e9972efa702f2010b5ad256d15df
Krishnanshu9 <krishnanshuchaudhary@gmail.com>
2025-10-23 05:55:12 +0530

    Default theme change

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,36 +3,36 @@ import { useEffect, useState } from "react"
 import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setDarkMode] = useState(true);
+    const [isLightMode, setLightMode] = useState(true);
     // useEffect is used for "Do something after render, and optionally clean it up."
-    useEffect(()=>{
+    useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
-        if(storedTheme=== "dark"){
-            document.documentElement.classList.add("dark");
-            setDarkMode(true);
-        }
-        else{
-            document.documentElement.classList.remove("dark");
-            setDarkMode(false);
-        }
-    },[]) //can also add dependencies
-    const toggleTheme = () => {
-        if (isDarkMode) {
-            document.documentElement.classList.remove("dark");
-            localStorage.setItem("theme","light");
-            setDarkMode(false);
+        if (storedTheme === "light") {
+            document.documentElement.classList.add("light");
+            setLightMode(true);
         }
         else {
-            document.documentElement.classList.add("dark");
-            localStorage.setItem("theme","dark");
-            setDarkMode(true);
+            document.documentElement.classList.remove("light");
+            setLightMode(false);
+        }
+    }, []) //can also add dependencies
+    const toggleTheme = () => {
+        if (isLightMode) {
+            document.documentElement.classList.remove("light");
+            localStorage.setItem("theme", "light");
+            setLightMode(false);
+        }
+        else {
+            document.documentElement.classList.add("light");
+            localStorage.setItem("theme", "light");
+            setLightMode(true);
         }
     }
     return <button onClick={toggleTheme} className={cn(" max-sm:hidden top-5 right-5 z-50",
-    "rounded-full transition-colors duration-300 focus:outline-hidden")}>
+        "rounded-full transition-colors duration-300 focus:outline-hidden")}>
         {/*only ternary opertor be used because "if" is a "statement", not an "expression", and 
         JSX can only contain "expressions" inside { } like ternary operator. */}
-        {isDarkMode ?
+        {isLightMode ?
             (<Sun className="h-6 w-6 text-yellow-400" />
             ) : (
                 <Moon className="h-6 w-6 text-blue-900" />)}
